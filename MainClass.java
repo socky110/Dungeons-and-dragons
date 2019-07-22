b2165f02a0de2722650226527e83610d2ce6143c
socky110 <socky110@gmail.com>
2019-07-22 14:34:12 -0600

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,3 @@
+import javax.swing.*; import java.awt.event.*;public class MainClass extends JApplet{ public MainClass(){} public static MainClass mc = new MainClass(); public static void main(String[] a){ } public void init(){ JButton jb1 = new JButton("file"); this.getContentPane().add(jb1); this.setVisible(true); }}
+
+ 
