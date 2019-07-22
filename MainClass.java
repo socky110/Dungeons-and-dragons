624bc8519e4a44917f074c1616e23eb7f15c1e6b
socky110 <socky110@gmail.com>
2019-07-22 14:43:11 -0600

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,3 +1,14 @@
-import javax.swing.*; import java.awt.event.*;public class MainClass extends JApplet{ public MainClass(){} public static MainClass mc = new MainClass(); public static void main(String[] a){ } public void init(){ JButton jb1 = new JButton("file"); this.getContentPane().add(jb1); this.setVisible(true); }}
+import javax.swing.*;
+import java.awt.event.*;
+public class MainClass extends JApplet{ 
+  public MainClass(){} 
+  public static MainClass mc = new MainClass(); 
+  public static void main(String[] a){ } 
+  public void init(){ 
+    JButton jb1 = new JButton("file");
+    this.getContentPane().add(jb1);
+    this.setVisible(true);
+  }
+}
 
  
